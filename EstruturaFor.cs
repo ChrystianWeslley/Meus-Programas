b45f34f77cbd6cea7f065abc08645dbc6c9853f3
Chrystian Weslley <113247141+ChrystianWeslley@users.noreply.github.com>
2022-12-21 19:40:37 -0300

    Add files via upload

---
+++ EstruturaFor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CursoCSharp.EstruturasDeControle {
+    internal class EstruturaFor {
+        public static void Executar() {
+
+            for  (int i = 0; i <= 10; i++) 
+            {
+                Console.WriteLine($"O valor de i é {i}");
+            }
+        }
+    }
+}
